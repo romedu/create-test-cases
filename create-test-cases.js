a2adc3e496884bb38d6226dbd6018bcf1ed0a6cc
romedu <romedu1@hotmail.com>
2020-02-09 23:12:08 -0400

    Redesign the createTestCases function

--- create-test-cases.js
+++ create-test-cases.js
@@ -1,12 +1,20 @@
-const createTestCases = (testData, fnToTest, matcher = "toBe") => {
+const createTestCases = (testData, fnToTest) => {
 	return testData.forEach(testCase => {
-		const { inputs, expectedOutput } = testCase,
+		const { inputs, expectedOutput, matchers = ["toBe"] } = testCase,
 			currentDescribeLabel = `Input: ${JSON.stringify(...inputs)}`;
 
 		describe(currentDescribeLabel, () => {
 			it(`should return ${expectedOutput}`, () => {
-				const output = fnToTest(...inputs);
-				expect(output)[matcher](expectedOutput);
+				const output = fnToTest(...inputs),
+					expectationObj = expect(output),
+					expectationMatcher = matchers.reduce(
+						(expectation, nextMatcher) => {
+							return expectation[nextMatcher];
+						},
+						expectationObj
+					);
+
+				expectationMatcher.call(expectationObj, expectedOutput);
 			});
 		});
 	});
