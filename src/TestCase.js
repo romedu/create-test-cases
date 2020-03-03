9ca173f002d6a8f08fee987c3b843216a3b2c2e6
romedu <romedu1@hotmail.com>
2020-03-02 23:47:21 -0400

    Fix a bug regarding the TestCase creator function

--- src/TestCase.js
+++ src/TestCase.js
@@ -3,8 +3,8 @@
 function TestCase(inputs, expectedOutput, matchers, testName) {
 	const newTestCase = this || {};
 
-	newTestCase.inputs = inputs || [];
-	newTestCase.expectedOutput = expectedOutput || null;
+	newTestCase.inputs = inputs;
+	newTestCase.expectedOutput = expectedOutput;
 	newTestCase.matchers = matchers || ["toBe"];
 	newTestCase.testName = testName || `should return ${newTestCase.expectedOutput}`;
 
