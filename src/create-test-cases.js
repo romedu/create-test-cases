b9ca7de9cfa75b9f7673540c42085873f91d96c6
romedu <romedu1@hotmail.com>
2020-03-02 22:17:24 -0400

    Format the createTestCases function

--- src/create-test-cases.js
+++ src/create-test-cases.js
@@ -1,25 +1,25 @@
 const createTestCases = (testData, fnToTest) => {
 	return testData.forEach(testCase => {
-		const { inputs, expectedOutput, testName, matchers = ["toBe"] } = testCase,
-			currentDescribeLabel = `Input: ${JSON.stringify(...inputs)}`,
-			currentTestName = testName || `should return ${expectedOutput}`;
+		const { inputs, expectedOutput, testName, matchers = ["toBe"] } = testCase;
+		const currentDescribeLabel = `Input: ${JSON.stringify(...inputs)}`;
+		const currentTestName = testName || `should return ${expectedOutput}`;
 
 		let matcherToUse;
 
 		describe(currentDescribeLabel, () => {
 			it(currentTestName, () => {
-				const output = fnToTest(...inputs),
-					initialExpectator = expect(output),
-					expectator = matchers.reduce((expectator, nextMatcher) => {
-						const nextExpectator = expectator[nextMatcher],
-							isExpectatorAMatcher = typeof nextExpectator === "function";
+				const output = fnToTest(...inputs);
+				const initialExpectator = expect(output);
+				const expectator = matchers.reduce((expectator, nextMatcher) => {
+					const nextExpectator = expectator[nextMatcher];
+					const isExpectatorAMatcher = typeof nextExpectator === "function";
 
-						if (!isExpectatorAMatcher) return nextExpectator;
-						else {
-							matcherToUse = nextExpectator;
-							return expectator;
-						}
-					}, initialExpectator);
+					if (!isExpectatorAMatcher) return nextExpectator;
+					else {
+						matcherToUse = nextExpectator;
+						return expectator;
+					}
+				}, initialExpectator);
 
 				matcherToUse.call(expectator, expectedOutput);
 			});
