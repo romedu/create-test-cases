ccb757b3fa5913db617ebc813c2ba232ee5ba3de
lervin <lervin100@hotmail.com>
2020-03-07 10:08:05 -0500

    handled case when TestCase inputs paramenter is undefined

--- spec/create-test-cases.spec.js
+++ spec/create-test-cases.spec.js
@@ -2,13 +2,13 @@ const createTestCases = require("../src/create-test-cases");
 const TestCase = require("../src/TestCase");
 
 describe("Create test cases function", () => {
-	const testData = [
-		new TestCase([true], true, ["toBe"]),
-		new TestCase([false], true, ["not", "toBe"]),
-		new TestCase(["Hello"], "Hello"),
-		new TestCase([44], 44)
-	];
-	const testFunction = input => input;
+  const testData = [
+    new TestCase([true], true, ["toBe"]),
+    new TestCase([false], true, ["not", "toBe"]),
+    new TestCase(["Hello"], "Hello"),
+    new TestCase([44], 44)
+  ];
+  const testFunction = input => input;
 
-	createTestCases(testData, testFunction);
+  createTestCases(testData, testFunction);
 });
--- src/utilities.js
+++ src/utilities.js
@@ -1,9 +1,19 @@
-exports.createInputsString = inputs => inputs.map(this.getInputStringValue).join(", ");
+exports.createInputsString = inputs => {
+  try {
+    inputs.map(this.getInputStringValue).join(", ");
+  } catch (e) {
+    const err =
+      "TestCase inputs argument shouldn't be undefined. Refer to https://github.com/romedu/create-test-cases";
+    throw err;
+  }
+};
 
 exports.getInputStringValue = input => {
-	const isInputAFunction = typeof input === "function";
-	const inputStringValue = isInputAFunction ? "FUNCTION" : JSON.stringify(input);
-	return inputStringValue;
+  const isInputAFunction = typeof input === "function";
+  const inputStringValue = isInputAFunction
+    ? "FUNCTION"
+    : JSON.stringify(input);
+  return inputStringValue;
 };
 
 module.exports = exports;
